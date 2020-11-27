1093de5ac29ed289bdf1c1fca7fbce0110a19e33
MSI\USER <infinitivea@gmail.com>
2020-11-27 15:58:00 +0700

    Integrate PregnantHistory

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import PrivateRoutes from './containers/PrivateRoutes';
 import LocalStorage from './services/localStorage';
-<<<<<<< HEAD
 import CurrentPregContext from './context/CurrentPregContext';
-=======
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Layout from './components/Layout/index';
-import Login from './pages/Login/index';
-import MotherRegister from './pages/MotherRegister/index';
->>>>>>> ea3f5a5474cabf23aeea3d5c09d2e403e6aeeea3
 
 function App() {
   const [mother, setMother] = useState({ currentPregId: '', idCard: '', Name: '', lastName: '', GA: 0 });
@@ -17,16 +10,9 @@ function App() {
 
   return (
     <div className="App" style={{ width: '100vw' }}>
-<<<<<<< HEAD
       <CurrentPregContext.Provider value={{ mother, setMother }}>
         <PrivateRoutes role={role} setRole={setRole} />
       </CurrentPregContext.Provider>
-=======
-      <PrivateRoutes role={role} setRole={setRole} />
-      <Layout>
-        <MotherRegister />
-      </Layout>
->>>>>>> ea3f5a5474cabf23aeea3d5c09d2e403e6aeeea3
     </div>
   );
 }
--- src/pages/MotherInformation/components/PregnantHistory.js
+++ src/pages/MotherInformation/components/PregnantHistory.js
@@ -1,75 +1,78 @@
+import axios from 'axios';
 import { Button, Table } from 'antd';
 import { PlusCircleOutlined } from '@ant-design/icons';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function PregnantHistory() {
-  const [data, setData] = useState({
-    dataSource: [
-      {
-        key: '1',
-        deliverDate: '17/11/2019',
-        ga: '32',
-        deliverMethod: 'คลอดธรรมชาติ',
-        childWeight: '330',
-        childGender: 'ชาย',
-        birthPlace: 'โรงพยาบาลกรุงเทพ',
-        complication: '-',
-        childStatus: 'ปกติ',
-      },
-    ],
-    count: 1,
-  });
+  const [data, setData] = useState({});
+
+  console.log(data);
+  useEffect(() => {
+    axios.get('/motherInformation/pregnantHistory?motherId=1').then((res) => {
+      setData({ dataSource: res.data, count: res.data.length });
+    });
+  }, []);
+
   const columns = [
     {
       title: 'ครรภ์ที่',
-      dataIndex: 'key',
+      dataIndex: 'id',
+      key: 'id',
     },
     {
       title: 'ว/ด/ป คลอด/แท้ง',
       dataIndex: 'deliverDate',
+      key: 'deliverDate',
     },
     {
       title: 'อายุครรภ์ (สัปดาห์)',
-      dataIndex: 'ga',
+      dataIndex: 'birthAtGa',
+      key: 'birthAtGa',
     },
     {
       title: 'วิธีคลอด/แท้ง',
       dataIndex: 'deliverMethod',
+      key: 'deliverMethod',
     },
     {
       title: 'น้ำหนักทารก',
       dataIndex: 'childWeight',
+      key: 'childWeight',
     },
     {
       title: 'เพศ',
       dataIndex: 'childGender',
+      key: 'childGender',
     },
     {
       title: 'สถานที่คลอด/แท้ง',
       dataIndex: 'birthPlace',
+      key: 'birthPlace',
     },
     {
       title: 'ภาวะแทรกซ้อน',
       dataIndex: 'complication',
+      key: 'complication',
     },
     {
       title: 'สภาพทารกปัจจุบัน',
       dataIndex: 'childStatus',
+      key: 'childStatus',
     },
   ];
 
   const handleAdd = () => {
     const { count, dataSource } = data;
     const newData = {
-      key: count + 1,
-      deliverDate: '17/11/2019',
-      ga: '32',
+      id: count + 1,
+      deliverDate: '17-11-2019',
+      birthAtGa: '32',
       deliverMethod: 'คลอดธรรมชาติ',
       childWeight: '330',
       childGender: 'ชาย',
       birthPlace: 'โรงพยาบาลกรุงเทพ',
       complication: '-',
-      childStatus: 'ปกติ',
+      childStatus: 'มีชีวิตอยู่',
     };
     setData({
       dataSource: [...dataSource, newData],
